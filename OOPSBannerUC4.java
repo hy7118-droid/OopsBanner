92e9e2c7afac24b7bf0639f34260044c4a052399
hy7118-droid <hy7118@srmist.edu.in>
2026-03-03 13:11:23 +0530

    Create OOPSBannerUC4.java

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,17 @@
+public class OOPSBannerUC4 {
+    public static void main(String[] args) {
+
+        String[] banner = {
+            "*****",
+            "*             *",
+            "*   HELLO!    *",
+            "*   UC4 APP   *",
+            "*             *",
+            "*****"
+        };
+
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
+    }
+}
